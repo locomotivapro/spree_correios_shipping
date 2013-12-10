2be82bfbc70267e10476b0faaf79274e5eadf612
Denis Tierno <de.tierno@gmail.com>
2013-12-10 16:05:47 -0200

    cath all errors in in compute method and return nil

--- app/models/spree/prices_correio.rb
+++ app/models/spree/prices_correio.rb
@@ -9,27 +9,28 @@ module Spree
     belongs_to :order
     before_save :compute
 
-  private
+    private
 
     def compute
       services = Spree::CorreiosShipping::Config[:services].split(',')
       services.map! { |s| s.strip.to_sym }
       count = 0
       frete = Correios::Frete::Calculador.new :cep_origem => Spree::CorreiosShipping::Config[:origin_zip_code],
-                                              :cep_destino => zipcode,
-                                              :peso => weight,
-                                              :comprimento => 30,
-                                              :largura => 15,
-                                              :altura => 2,
-                                              :codigo_empresa => Spree::CorreiosShipping::Config[:id_correios],
-                                              :senha => Spree::CorreiosShipping::Config[:password_correios]
+        :cep_destino => zipcode,
+        :peso => weight,
+        :comprimento => 30,
+        :largura => 15,
+        :altura => 2,
+        :codigo_empresa => Spree::CorreiosShipping::Config[:id_correios],
+        :senha => Spree::CorreiosShipping::Config[:password_correios]
+
       response = begin
-      servico = frete.calcular *services
-      rescue Errno::ECONNRESET => e
-        count += 1
-        retry unless count > 3
-        nil
-      end
+                   servico = frete.calcular *services
+                 rescue => e
+                   count += 1
+                   retry unless count > 3
+                   nil
+                 end
 
       unless response.nil?
         response_hash = {}
--- spree_correios_shipping.gemspec
+++ spree_correios_shipping.gemspec
@@ -2,7 +2,7 @@
 Gem::Specification.new do |s|
   s.platform    = Gem::Platform::RUBY
   s.name        = 'spree_correios_shipping'
-  s.version     = '1.1.7'
+  s.version     = '1.1.8'
   s.summary     = 'Gem for spree and correios'
   #s.description = 'Add (optional) gem description here'
   s.required_ruby_version = '>= 1.8.7'
