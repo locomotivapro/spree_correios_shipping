1cfbfc8e8d11b62350b133f7023dd4a8b87770c4
Denis Tierno <de.tierno@gmail.com>
2015-12-04 12:06:22 -0200

    correct tranlation and change gem version to 1.1.0

--- app/models/spree/shipping_rate_decorator.rb
+++ app/models/spree/shipping_rate_decorator.rb
@@ -1,8 +1,9 @@
 Spree::ShippingRate.class_eval do
 
   def timing_info
-    if days
-      text = [Spree.t(:pre_delivery_days_info), days, Spree.t(:delivery_days)]
+    if days || info
+      text = [Spree.t(:pre_delivery_days_info)]
+      text << "#{days} #{Spree.t(:delivery_days)}" if days
       text << "(#{info})" if info
       text.join(' ')
     end
